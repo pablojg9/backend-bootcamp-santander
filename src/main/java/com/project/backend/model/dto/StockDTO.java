c80ce3c7225a073fd52fefad72125da61042efae
pablojg9 <pablojuniorgn2@gmail.com>
2021-05-26 22:16:20 -0300

    API REST - 26/05/2021

--- src/main/java/com/project/backend/model/dto/StockDTO.java
+++ src/main/java/com/project/backend/model/dto/StockDTO.java
@@ -1,14 +1,31 @@
 package com.project.backend.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class StockDTO {
 
     //Attributes
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
+    @DecimalMax(value = "0.00")
+    @Digits(integer = 6, fraction = 2)
     private Double price;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate date;
+
+    @NotNull
+    @Digits(integer = 3, fraction = 2)
     private Double variation;
 
     // Getters e Setters
