19caa9f5c6a895f3e8329805daa6b5313329adbc
pablojg9 <pablojuniorgn2@gmail.com>
2021-05-29 14:05:15 -0300

    API REST - JPA - 29/05/2021

---
+++ src/main/java/com/project/backend/repository/StockRepository.java
@@ -0,0 +1,11 @@
+package com.project.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends JpaRepository {
+
+
+
+}
